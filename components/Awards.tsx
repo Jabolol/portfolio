e77423556a0a36bab194023283f22b13c297615f
Javier R <javier.rios-urbano@epitech.eu>
2023-09-23 18:38:54 +0200

    chore: add won `hackathons`

--- components/Awards.tsx
+++ components/Awards.tsx
@@ -1,48 +1,58 @@
 import Trophy from "$icons/trophy.tsx";
-import { JSX } from "preact/jsx-runtime";
+import Card from "~/components/Card.tsx";
+import { CardProps } from "~/types.ts";
 
-const Award = (
-  { title, description, Component, inject, link }: {
-    title: string;
-    description: string;
-    Component: ({ ...props }) => JSX.Element;
-    inject: string;
-    link: string;
+const details: (CardProps & { link: string })[] = [
+  {
+    title: "XDC Foundation",
+    description: "1st place in the Social Media Crypto Bot category.",
+    Component: Trophy,
+    inject: "text-yellow-500",
+    link:
+      "https://www.linkedin.com/posts/javierriosur_last-week-i-had-the-opportunity-to-participate-activity-7084512255098126336-hiaL",
   },
-) => (
-  <a href={link} target="_blank" rel="noopener noreferrer">
-    <div class="flex flex-col items-center">
-      <Component class={`h-10 w-auto m-5 ${inject}`} />
-      <h1 class="text-1xl font-bold">{title}</h1>
-      <p class="text-gray-400 text-center m-2 max-w-[16rem]">
-        {description}
-      </p>
-    </div>
-  </a>
-);
-
-const details: Readonly<Parameters<typeof Award>[0][]> = [{
-  title: "Mediamarkt Hackathon",
-  description: "1st place building a mobile app and blockchain PoC.",
-  Component: Trophy,
-  inject: "text-yellow-500",
-  link:
-    "https://www.linkedin.com/posts/javierriosur_letsgomediamarkt-mediamarktsaturntechnology-activity-7052268380841553921-7OsO",
-}, {
-  title: "NEAR Web3 Hackathon",
-  description: "1st place building a decentralised certification storage.",
-  Component: Trophy,
-  inject: "text-yellow-400",
-  link:
-    "https://www.linkedin.com/posts/alex-arteaga-c_honored-to-have-participated-in-this-weekends-activity-7053675065107750913-6uq4",
-}, {
-  title: "IBM Metaverse Hackathon",
-  description: "2nd place creating a Metaverse demo using Unity.",
-  Component: Trophy,
-  inject: "text-gray-400",
-  link:
-    "https://www.linkedin.com/posts/javierriosur_this-sunday-the-metaverse-social-club-hackathon-activity-7003371720011059200-Ri0_",
-}];
+  {
+    title: "Mediamarkt Hackathon",
+    description:
+      "1st place building a mobile app and blockchain Proof of Concept.",
+    Component: Trophy,
+    inject: "text-yellow-500",
+    link:
+      "https://www.linkedin.com/posts/javierriosur_letsgomediamarkt-mediamarktsaturntechnology-activity-7052268380841553921-7OsO",
+  },
+  {
+    title: "Google & Interacso Open Source Jam",
+    description: "1st place contributing to open-source projects on GitHub.",
+    Component: Trophy,
+    inject: "text-yellow-500",
+    link:
+      "https://www.linkedin.com/posts/javierriosur_thrilled-to-share-that-i-emerged-as-a-winner-activity-7078410738955882497-ciRz",
+  },
+  {
+    title: "NEAR Web3 Hackathon",
+    description: "1st place building a decentralised certification storage.",
+    Component: Trophy,
+    inject: "text-yellow-400",
+    link:
+      "https://www.linkedin.com/posts/alex-arteaga-c_honored-to-have-participated-in-this-weekends-activity-7053675065107750913-6uq4",
+  },
+  {
+    title: "ETH Barcelona Hackathon",
+    description: "Best Decentralized Finance project in the hackathon.",
+    Component: Trophy,
+    inject: "text-yellow-500",
+    link:
+      "https://www.linkedin.com/posts/alex-arteaga-c_great-news-im-elated-to-share-with-activity-7084461331734511616-0y0g",
+  },
+  {
+    title: "IBM Metaverse Hackathon",
+    description: "2nd place creating a Metaverse demo using Unity.",
+    Component: Trophy,
+    inject: "text-gray-400",
+    link:
+      "https://www.linkedin.com/posts/javierriosur_this-sunday-the-metaverse-social-club-hackathon-activity-7003371720011059200-Ri0_",
+  },
+];
 
 export default function Awards() {
   return (
@@ -56,13 +66,22 @@ export default function Awards() {
           <h1 class="font-black text-dim text-4xl mt-10 text-3xl">
             Awards
           </h1>
-          <p class="mt-4 text-lg lg:text-2xl text-gray-400 max-w-[18rem] md:max-w-[30rem] lg:max-w-[60rem]">
+          <p class="mt-4 text-lg lg:text-2xl dark:text-gray-400 text-gray-600 max-w-[18rem] md:max-w-[30rem] lg:max-w-[60rem]">
             Some of my hackathon feats.
           </p>
         </div>
       </div>
       <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center mb-5 md:mb-15 lg:mb-15">
-        {details.map((props) => <Award {...props} />)}
+        {details.map(({ link, title, description, Component, inject }) => (
+          <a href={link} target="_blank" rel="noopener noreferrer">
+            <Card
+              title={title}
+              description={description}
+              Component={Component}
+              inject={inject}
+            />
+          </a>
+        ))}
       </div>
     </>
   );
