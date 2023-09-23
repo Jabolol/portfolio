c8245431d82fef9bb472c9f14e6c72474851a1bb
Javier R <javier.rios-urbano@epitech.eu>
2023-09-24 00:00:16 +0200

    fix: use `fadeIn` hook to improve lighthouse score

--- islands/Contributions.tsx
+++ islands/Contributions.tsx
@@ -2,6 +2,7 @@ import GitHubCalendar from "github-calendar";
 import { Activity } from "activity-calendar";
 import useDarkMode from "~/hooks/darkMode.ts";
 import useWidth from "~/hooks/width.ts";
+import useFadeIn from "~/hooks/fadeIn.ts";
 
 const selectLastHalfYear = (contributions: Activity[]) =>
   contributions.filter(
@@ -14,6 +15,7 @@ const selectLastHalfYear = (contributions: Activity[]) =>
 export default function Contributions() {
   const { mode } = useDarkMode();
   const { width, breakpoint, getWidth } = useWidth();
+  const { isLoaded, ref } = useFadeIn<HTMLDivElement>();
   const isReduced = width.value < getWidth(breakpoint.value);
 
   return (
@@ -22,22 +24,25 @@ export default function Contributions() {
         class="w-48 h-1 mt-12 mx-auto bg-gray-100 border-0 rounded md:my-10 dark:bg-gray-700"
         id="contact"
       />
-      <div class="flex items-center justify-center my-5">
+      <div ref={ref} class="flex items-center justify-center my-5">
         <div class="mx-auto lg:px-6 lg:text-center text-center md:pb-5 lg:pb-5">
           <h1 class="font-black text-dim text-4xl mt-10 text-3xl">
             Contributions
           </h1>
           <p class="mt-4 text-lg lg:text-2xl dark:text-gray-400 text-gray-600 max-w-[18rem] md:max-w-[30rem] lg:max-w-[60rem]">
-            GitHub graph for the last{" "}
-            {isReduced ? "6 months" : "year"}.
+            GitHub graph for the last {isReduced ? "6 months" : "year"}.
           </p>
         </div>
       </div>
-      <div class="flex justify-center items-center mb-5 md:mb-15 lg:mb-15 overflow-x-scroll mx-10">
+      <div
+        class={`${
+          isLoaded.value ? "opacity-100" : "opacity-0"
+        } transition-opacity duration-500 text-center flex justify-center items-center mb-5 md:mb-15 lg:mb-15 overflow-x-scroll mx-10`}
+      >
         <GitHubCalendar
           username={"jabolol"}
           colorScheme={mode.value}
-          transformData={isReduced? selectLastHalfYear: undefined}
+          transformData={isReduced ? selectLastHalfYear : undefined}
           labels={{ totalCount: "{{count}} contributions" }}
         />
       </div>
