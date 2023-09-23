e4a19e4355cbabf41ba8459c4ff366e73cc05114
Javier R <javier.rios-urbano@epitech.eu>
2023-09-23 17:49:39 +0200

    add: `html` structure

---
+++ routes/_app.tsx
@@ -0,0 +1,21 @@
+import { AppProps } from "$fresh/server.ts";
+
+export default function App({ Component }: AppProps) {
+  return (
+    <html>
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="Explore Javi's portfolio and discover his latest projects in full stack development and design."
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <title>portfolio</title>
+      </head>
+      <body>
+        <Component />
+      </body>
+    </html>
+  );
+}
