f3fdae3c2a586db14cc424229651954fe264ca26
Javier R <javier.rios-urbano@epitech.eu>
2023-09-24 21:28:34 +0200

    add: `activity-calendar` types

--- types.ts
+++ types.ts
@@ -1,8 +1,23 @@
 import { JSX } from "preact";
+import { Activity } from "activity-calendar";
 
 export type CardProps = {
   title: string;
   description: string;
   Component: ({ ...props }) => JSX.Element;
   inject: string;
+};
+
+export type Year = number | "last";
+
+export type ApiResponse = {
+  total: {
+    [year: number]: number;
+    [year: string]: number;
+  };
+  contributions: Array<Activity>;
+};
+
+export type ApiErrorResponse = {
+  error: string;
 };
