f10772d8ee4da83cd85e804a24f7343098dd0fd9
Javier R <javier.rios-urbano@epitech.eu>
2023-09-26 10:24:45 +0200

    fix: avoid `duplicate` effects, match `prefers-color-scheme`

--- hooks/darkMode.ts
+++ hooks/darkMode.ts
@@ -1,18 +1,26 @@
 import { computed, signal, useSignalEffect } from "@preact/signals";
 import { IS_BROWSER } from "$fresh/runtime.ts";
 
+const isListening = signal<boolean>(false);
 const isDarkMode = signal<boolean>(false);
 
 const useDarkMode = () => {
-  const toggle = () => {
-    isDarkMode.value = !isDarkMode.value;
-  };
+  const toggle = () => isDarkMode.value = !isDarkMode.value;
 
-  useSignalEffect(() => {
-    if (IS_BROWSER) {
+  if (!isListening.value && IS_BROWSER) {
+    isDarkMode.value =
+      window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    useSignalEffect(() => {
       document.documentElement.classList.toggle("dark", isDarkMode.value);
-    }
-  });
+
+      return () => {
+        isListening.value = false;
+      };
+    });
+  }
+
+  isListening.value = true;
 
   const mode = computed(() => isDarkMode.value ? "dark" : "light");
 
