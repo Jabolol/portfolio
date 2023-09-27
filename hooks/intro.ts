d6a9528ba878a0e2e9213d5e790f14fe7e0e0bb3
Javier R <javier.rios-urbano@epitech.eu>
2023-09-27 11:13:08 +0200

    fix: avoid `duplicate` phrases

--- hooks/intro.ts
+++ hooks/intro.ts
@@ -1,8 +1,9 @@
 import { useSignal, useSignalEffect } from "@preact/signals";
 import { phrases } from "~/misc.ts";
 
-const getRandomPhrase = () => {
-  return phrases[Math.floor(Math.random() * phrases.length)];
+const getRandomPhrase = (prev = ""): string => {
+  const phrase = phrases[Math.floor(Math.random() * phrases.length)];
+  return phrase === prev ? getRandomPhrase(phrase) : phrase;
 };
 
 const useIntro = () => {
@@ -13,7 +14,7 @@ const useIntro = () => {
     const intervalId = setInterval(() => {
       isVisible.value = false;
       setTimeout(() => {
-        intro.value = getRandomPhrase();
+        intro.value = getRandomPhrase(intro.value);
         isVisible.value = true;
       }, 500);
     }, 3e3);
