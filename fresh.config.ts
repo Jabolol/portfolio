3aa5212352b8c2207a5b23596d6b2201e134c72b
Javier R <javier.rios-urbano@epitech.eu>
2025-12-07 18:11:59 +0100

    feat: remove Fresh `v1` legacy files

--- fresh.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "$fresh/src/server/defines.ts";
-import twindConfig from "~/twind.config.ts";
-import twindPlugin from "$fresh/plugins/twind.ts";
-import { ga4Plugin } from "ga4/mod.ts";
-
-export default defineConfig({
-  plugins: [twindPlugin(twindConfig), ga4Plugin()],
-});
